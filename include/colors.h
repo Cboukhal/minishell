56d2c61345ad162501608584387b731c14d9970c
Jeremy BOCKTOR <jbocktor@bess-f4r4s3.clusters.42paris.fr>
2024-04-16 12:05:52 +0200

    colors

--- include/colors.h
+++ include/colors.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   colors.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agadea <agadea@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jbocktor <jbocktor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 12:08:02 by agadea            #+#    #+#             */
-/*   Updated: 2023/07/02 19:47:00 by agadea           ###   ########.fr       */
+/*   Updated: 2024/04/16 11:37:55 by jbocktor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,49 +21,49 @@
 	hexadecimal	:	\x1B[m
 */
 
-# define DEFAULT "\033[0m"
-# define BOLD "\033[1m"
-# define ITALIC "\033[3m"
-# define SLOW_BLINK "\033[5m"
-# define RAPID_BLINK "\033[6m"
-# define FRAKTUR "\033[20m"
+# define DEFAULT "\1\033[0m\2"
+# define BOLD "\1\033[1m\2"
+# define ITALIC "\1\033[3m\2"
+# define SLOW_BLINK "\1\033[5m\2"
+# define RAPID_BLINK "\1\033[6m\2"
+# define FRAKTUR "\1\033[20m\2"
 
 /*		COLORS		*/
 
-# define BLACK "\033[30m"
-# define RED "\033[31m"
-# define GREEN "\033[32m"
-# define YELLOW "\033[33m"
-# define BLUE "\033[34m"
-# define MAGENTA "\033[35m"
-# define CYAN "\033[36m"
-# define WHITE "\033[37m"
+# define BLACK "\1\033[30m\2"
+# define RED "\1\033[31m\2"
+# define GREEN "\1\033[32m\2"
+# define YELLOW "\1\033[33m\2"
+# define BLUE "\1\033[34m\2"
+# define MAGENTA "\1\033[35m\2"
+# define CYAN "\1\033[36m\2"
+# define WHITE "\1\033[37m\2"
 
-# define BLACK_BACKGROUND "\033[40m"
-# define RED_BACKGROUND "\033[41m"
-# define GREEN_BACKGROUND "\033[42m"
-# define YELLOW_BACKGROUND "\033[43m"
-# define BLUE_BACKGROUND "\033[44m"
-# define MAGENTA_BACKGROUND "\033[45m"
-# define CYAN_BACKGROUND "\033[46m"
-# define WHITE_BACKGROUND "\033[47m"
+# define BLACK_BACKGROUND "\1\033[40m\2"
+# define RED_BACKGROUND "\1\033[41m\2"
+# define GREEN_BACKGROUND "\1\033[42m\2"
+# define YELLOW_BACKGROUND "\1\033[43m\2"
+# define BLUE_BACKGROUND "\1\033[44m\2"
+# define MAGENTA_BACKGROUND "\1\033[45m\2"
+# define CYAN_BACKGROUND "\1\033[46m\2"
+# define WHITE_BACKGROUND "\1\033[47m\2"
 
-# define BRIGHT_BLACK "\033[90m"
-# define BRIGHT_RED "\033[91m"
-# define BRIGHT_GREEN "\033[92m"
-# define BRIGHT_YELLOW "\033[93m"
-# define BRIGHT_BLUE "\033[94m"
-# define BRIGHT_MAGENTA "\033[95m"
-# define BRIGHT_CYAN "\033[96m"
-# define BRIGHT_WHITE "\033[97m"
+# define BRIGHT_BLACK "\1\033[90m\2"
+# define BRIGHT_RED "\1\033[91m\2"
+# define BRIGHT_GREEN "\1\033[92m\2"
+# define BRIGHT_YELLOW "\1\033[93m\2"
+# define BRIGHT_BLUE "\1\033[94m\2"
+# define BRIGHT_MAGENTA "\1\033[95m\2"
+# define BRIGHT_CYAN "\1\033[96m\2"
+# define BRIGHT_WHITE "\1\033[97m\2"
 
-# define BRIGHT_BLACK_BACKGROUND "\033[100m"
-# define BRIGHT_RED_BACKGROUND "\033[101m"
-# define BRIGHT_GREEN_BACKGROUND "\033[102m"
-# define BRIGHT_YELLOW_BACKGROUND "\033[103m"
-# define BRIGHT_BLUE_BACKGROUND "\033[104m"
-# define BRIGHT_MAGENTA_BACKGROUND "\033[105m"
-# define BRIGHT_CYAN_BACKGROUND "\033[106m"
-# define BRIGHT_WHITE_BACKGROUND "\033[107m"
+# define BRIGHT_BLACK_BACKGROUND "\1\033[100m\2"
+# define BRIGHT_RED_BACKGROUND "\1\033[101m\2"
+# define BRIGHT_GREEN_BACKGROUND "\1\033[102m\2"
+# define BRIGHT_YELLOW_BACKGROUND "\1\033[103m\2"
+# define BRIGHT_BLUE_BACKGROUND "\1\033[104m\2"
+# define BRIGHT_MAGENTA_BACKGROUND "\1\033[105m\2"
+# define BRIGHT_CYAN_BACKGROUND "\1\033[106m\2"
+# define BRIGHT_WHITE_BACKGROUND "\1\033[107m\2"
 
 #endif
